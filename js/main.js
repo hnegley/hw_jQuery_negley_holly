de246bd0def01a8376cb86f96ec77183f7876ca5
Holly Negley <holly.negley@icloud.com>
2015-03-21 10:29:08 -0400

    CSS changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-$( document ).ready(function() {
-       var headings = $('#wikiArticle').find('h2'){
-        console.log ("I'm Holly Negley, and I'm learning JQuery!")
-    }});
+// $( document ).ready(function(){
+ //      var headings = $('#wikiArticle').find('h2'){
+ //       console.log ("I'm Holly Negley, and I'm learning JQuery!")
+ //   }});
 
+$( document ).ready(function() {
+       var paragraphs = $('#wikiArticle').find('p').css('color', 'red', 'size', '2em', 'font-weight', 'strong');
+       });
